1e95b4d413787c988a44059e050fc2462329d487
hsiehyunju <hsiehyunju@gmail.com>
2023-12-21 23:15:01 +0800

    feat: TourListAdapter 加入 ClickListener

--- app/src/main/java/com/example/taipeitour/recycler/TourListAdapter.kt
+++ app/src/main/java/com/example/taipeitour/recycler/TourListAdapter.kt
@@ -1,12 +1,16 @@
 package com.example.taipeitour.recycler
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.taipeitour.R
 import com.example.taipeitour.data.TourItem
 import com.example.taipeitour.databinding.TourlistItemBinding
+import java.util.ArrayList
 
 /**
  * Tour List RecyclerView 使用的 Adapter
@@ -26,6 +30,19 @@ class TourListAdapter: ListAdapter<TourItem, TourListAdapter.TourViewHolder>(Tou
                 // 設定景點資訊
                 listTitle.text = data.toString()
                 listRemind.text = data.remind
+
+                this.root.setOnClickListener {
+                    var bundle = Bundle().apply {
+                        putString("title", data.toString())
+                        putString("introduction", data.introduction)
+                        putString("address", data.address)
+                        putString("tel", data.tel)
+                        putString("url", data.url)
+                        putStringArrayList("images", ArrayList(data.images))
+                    }
+
+                    Navigation.findNavController(binding.root).navigate(R.id.action_listFragment_to_detailFragment, bundle)
+                }
             }
         }
     }
