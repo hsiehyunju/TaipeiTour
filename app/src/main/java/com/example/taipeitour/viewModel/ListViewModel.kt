dc2dbf7caac29219af50d891d2f0d3d560fc907a
hsiehyunju <hsiehyunju@gmail.com>
2023-12-21 11:32:16 +0800

    feat: ListViewModel 先塞入測試資料

--- app/src/main/java/com/example/taipeitour/viewModel/ListViewModel.kt
+++ app/src/main/java/com/example/taipeitour/viewModel/ListViewModel.kt
@@ -13,11 +13,25 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
 
     init {
         // 初始化時下載數據
-        fetchData();
+        fetchData()
     }
 
     private fun fetchData()
     {
         // TODO: 透過 Api 抓取資料
+        var list = List(1) { index ->
+            TourItem(
+                id = index,
+                name = "景點 $index",
+                introduction = "景點 $index 的介紹",
+                remind = "景點 $index 的提醒",
+                url = "https://example.com",
+                address = "景點 $index 的地址",
+                tel = "景點 $index 的電話",
+                images = listOf("https://www.travel.taipei/image/221368")
+            )
+        }
+
+        _tourList.postValue(list)
     }
 }
