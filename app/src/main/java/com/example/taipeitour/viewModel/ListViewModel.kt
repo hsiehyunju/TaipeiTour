79e719784295e4a094a95ea0b3d6c5bb2d246c25
hsiehyunju <hsiehyunju@gmail.com>
2023-12-21 17:09:12 +0800

    feat: ListViewModel 抓取 Api 資料，並同步到 UI

--- app/src/main/java/com/example/taipeitour/viewModel/ListViewModel.kt
+++ app/src/main/java/com/example/taipeitour/viewModel/ListViewModel.kt
@@ -4,34 +4,86 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.taipeitour.data.TourItem
+import com.example.taipeitour.repository.TourRepository
+import com.example.taipeitour.tools.LogTool
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class ListViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val _tourListCache: MutableList<TourItem> = mutableListOf()
+
     private val _tourList = MutableLiveData<List<TourItem>>()
     val tourList: LiveData<List<TourItem>> get() = _tourList
+
+    private val repository = TourRepository()
+
+    /** 儲存呼叫 Api 次數  */
+    private var _callApiCount = 0
+    /** 單次 Api 回傳的最大資料量 */
+    private val _maxDataPerPage = 30
 
     init {
         // 初始化時下載數據
         fetchData()
     }
 
+    /**
+     * 抓取 Server 資料
+     */
     private fun fetchData()
     {
-        // TODO: 透過 Api 抓取資料
-        var list = List(1) { index ->
-            TourItem(
-                id = index,
-                name = "景點 $index",
-                introduction = "景點 $index 的介紹",
-                remind = "景點 $index 的提醒",
-                url = "https://example.com",
-                address = "景點 $index 的地址",
-                tel = "景點 $index 的電話",
-                images = listOf("https://www.travel.taipei/image/221368")
-            )
-        }
 
-        _tourList.postValue(list)
+        viewModelScope.launch {
+
+            val numberOfPage = _callApiCount + 1
+
+            try {
+                val response = repository.getTourDataRaw("zh-tw", numberOfPage)
+
+                if (response.isSuccessful)
+                {
+                    LogTool.i("response is successful, ths page is " + numberOfPage)
+                    response.body()?.let {
+                        for (item in it.data)
+                        {
+                            val tourItem = TourItem(
+                                id = item.id,
+                                name = item.name,
+                                introduction = item.introduction,
+                                remind = item.remind,
+                                url = item.url,
+                                tel = item.tel,
+                                address = item.address,
+                                images = item.images.map { image -> image.src }
+                            )
+
+                            _tourListCache.add(tourItem)
+                        }
+
+                        _tourList.postValue(_tourListCache)
+                        _callApiCount++
+
+                        if (it.total != _tourListCache.size)
+                        {
+                            fetchData()
+                        }
+                    }
+                }
+                else
+                {
+                    LogTool.e("response is not successful")
+                }
+
+            } catch (e: HttpException) {
+                val url = e.response()?.raw()?.request()?.url().toString()
+                val errorCode = e.code()
+                val errorMessage = e.message()
+                // 顯示錯誤消息，同時顯示 URL
+                println("HTTP error code: $errorCode, message: $errorMessage, URL: $url")
+            }
+        }
     }
 }
